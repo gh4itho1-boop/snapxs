f0b81f4c719507e7eea5eef8e297aa45b324e783
gh4itho1-boop <gh4itho1@gmail.com>
2026-05-14 17:05:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'overall.js'));
+  res.sendFile(path.join(__dirname, 'public', 'overall.html'));
 });
 
 const sessions = new Map();
